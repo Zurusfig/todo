17359e62ca640aae14eafa9e02f46badac0d3640
Zagif <47357346+Zurusfig@users.noreply.github.com>
2025-07-16 19:06:34 +0700

    some skeleton for main page done

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,3 @@
-// webpack.config.js
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
@@ -9,6 +8,24 @@ module.exports = {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
     clean: true,
+  },
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, "dist"),
+    },
+    watchFiles: ["src/**/*"],
+    open: true,
+    hot: true,
+    port: 3000,
+    liveReload: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
   },
   plugins: [
     new HtmlWebpackPlugin({
