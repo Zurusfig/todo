4cd55e92f9973c382921a79178145c8986ea8b3b
Ittichet Thongsang <zagif2151382@gmail.com>
2025-07-21 10:34:48 +0700

    nav panel added

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 import './style.css';
 
 console.log("Webpack is working!");
+
+const navIconInHeader = document.querySelector(".nav-icon");
+const navPanel = document.querySelector(".nav-panel");
+const main = document.querySelector('.main-body');
+
+navIconInHeader.addEventListener("click", (e) => {
+        console.log("display off off")
+        navPanel.classList.toggle("collapsed");
+        navIconInHeader.classList.toggle("hidden");
+        main.classList.toggle('expanded');
+})
+
+const navIconInPanel = document.querySelector(".panel-icon");
+
+navIconInPanel.addEventListener("click", (e) => {
+    navPanel.classList.toggle("collapsed");
+    navIconInHeader.classList.toggle("hidden");
+    main.classList.toggle('expanded');
+});
+
