bb2bada16a2fe27839eeda0685c940a5f858484f
Ittichet Thongsang <zagif2151382@gmail.com>
2025-07-23 17:40:41 +0700

    tick function

--- src/index.js
+++ src/index.js
@@ -21,3 +21,23 @@ navIconInPanel.addEventListener("click", (e) => {
     main.classList.toggle('expanded');
 });
 
+
+
+const taskItems = document.querySelectorAll(".task-item");
+
+taskItems.forEach(element => {
+    const taskItem = element;
+    const taskCircle = taskItem.querySelector(".task-circle");
+    const taskInfo = taskItem.querySelector(".task-info");
+    const taskStar = taskItem.querySelector(".task-star");
+    taskCircle.addEventListener("click", (e) => {
+        taskCircle.querySelector(".checked").classList.toggle('display-none');
+        taskCircle.querySelector(".unchecked").classList.toggle('display-none');
+        taskInfo.classList.toggle("cross-text");
+    });
+
+    taskStar.addEventListener("click", (e) => {
+        taskStar.querySelector(".material-symbols-outlined").classList.toggle("fill-star");
+    })
+
+});
