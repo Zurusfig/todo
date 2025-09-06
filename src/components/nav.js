ca3431b0c9e443af5c4dc166bec633823605ceb4
Ittichet Thongsang <zagif2151382@gmail.com>
2025-09-06 19:43:34 +0700

    Add navigation bar initialization and structure to the template

--- src/components/nav.js
+++ src/components/nav.js
@@ -1,3 +1,81 @@
+export function initNavBar(){
+    const wrapper = document.querySelector('.wrapper');
+    const navPanel = document.createElement('div');
+    navPanel.classList.add('nav-panel','collapsed');
+    wrapper.appendChild(navPanel);
+
+    const panelIcon = document.createElement('div');
+    panelIcon.classList.add('panel-icon');
+    const menuIcon = document.createElement('span');
+    menuIcon.classList.add('material-symbols-outlined');
+    menuIcon.textContent = "menu";
+    panelIcon.appendChild(menuIcon);
+    navPanel.appendChild(panelIcon);
+
+    const allTasksTab = document.createElement('div');
+    allTasksTab.classList.add('all-tasks','active');
+    allTasksTab.textContent = "ALL TASKS";
+    navPanel.appendChild(allTasksTab);
+
+    const defaultListTab = document.createElement('div');
+    defaultListTab.classList.add('default-list');
+    navPanel.appendChild(defaultListTab);
+
+    const todayTask = document.createElement('div');
+    todayTask.classList.add('list', 'clickable');
+    todayTask.textContent = "TODAY";
+    defaultListTab.appendChild(todayTask);
+
+    const upcomingTask = document.createElement('div');
+    upcomingTask.classList.add('list', 'clickable');
+    upcomingTask.textContent = "UPCOMING";
+    defaultListTab.appendChild(upcomingTask);
+
+    const importantTask = document.createElement('div');
+    importantTask.classList.add('list', 'clickable');
+    importantTask.textContent = "IMPORTANT";
+    defaultListTab.appendChild(importantTask);
+
+    const completedTask = document.createElement('div');
+    completedTask.classList.add('list', 'clickable');
+    completedTask.textContent = "COMPLETED";
+    defaultListTab.appendChild(completedTask);
+
+    const projectContainer = document.createElement('div');
+    projectContainer.classList.add('project-container');
+    navPanel.appendChild(projectContainer);
+
+    const projectHeader = document.createElement('div');
+    projectHeader.classList.add('project-header');
+    projectHeader.textContent = "PROJECTS";
+    projectContainer.appendChild(projectHeader);
+
+    const projectRectBar = document.createElement('div');
+    projectRectBar.classList.add('rectangle-bar');
+    projectContainer.appendChild(projectRectBar);
+
+    const projectLists = document.createElement('div');
+    projectLists.classList.add('project-lists');
+    projectContainer.appendChild(projectLists);
+
+    // potentially add project lists here later
+
+    const addProjectBtn = document.createElement('div');
+    addProjectBtn.classList.add('add-project-btn');
+    const addIcon = document.createElement('span');
+    addIcon.classList.add('material-symbols-outlined');
+    addIcon.textContent = "add_circle";
+    addProjectBtn.appendChild(addIcon);
+    const addText = document.createElement('div');
+    addText.classList.add('text');
+    addText.textContent = "ADD PROJECT";
+    addProjectBtn.appendChild(addText);
+
+    projectContainer.appendChild(addProjectBtn);
+
+    console.log("nav bar initiated");
+}
+
 function initNavToggle(){
     const navIconInHeader = document.querySelector(".nav-icon");
     const navPanel = document.querySelector(".nav-panel");
--- src/index.js
+++ src/index.js
@@ -1,15 +1,18 @@
 import './style.css';
-import initNavToggle from './components/nav.js';
+import initNavToggle, { initNavBar } from './components/nav.js';
 import { initTaskInteractions, createTaskElement } from './components/taskItem.js';
 import { initOverlayToggle } from './components/modal.js'
 import './forms/formHandler.js';
 import { allTasks } from './utils/app.js';
+
+initNavBar();
 
 initNavToggle();
 
 initTaskInteractions();
 
 initOverlayToggle();
+
 
 const allTaskTitle = document.querySelector(".main-body .title");
 allTaskTitle.addEventListener("click",(e)=>{
